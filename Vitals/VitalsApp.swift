8b297f28d65800aff1991c5cffe816c8d4780d37
Udean777 <uaart123@gmail.com>
2026-06-13 20:14:46 +0700

    Initial Commit

---
+++ Vitals/VitalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitalsApp.swift
+//  Vitals
+//
+//  Created by Sajudin on 13/06/26.
+//
+
+import SwiftUI
+
+@main
+struct VitalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
